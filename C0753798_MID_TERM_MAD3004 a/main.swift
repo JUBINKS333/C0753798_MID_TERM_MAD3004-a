dcf0cf8cbd7c45807c0a77af091a0bab10165692
MacStudent <macstudent@126-imac29.local>
2019-02-20 20:08:20 -0500

    orders using dictionary

--- C0753798_MID_TERM_MAD3004 a/main.swift
+++ C0753798_MID_TERM_MAD3004 a/main.swift
@@ -11,24 +11,19 @@ import Foundation
 print("----------------------------------------------")
 
 
-/*
-var Products
-
-
-p.append("P001","Hard drive",120.00)
-p.append("P002","ZIP drive", 90.00)
-p.append("P003","Floppy disk", 50.00)
-p.append("P004","Monitor",300.00)
-p.append("P005"," iPhone 7 Plus",1200.00)
-
-p.display()
- */
 
 var p1=Product(PId:"P001", Pname:"Hard drive", prce: 120.00, qnty: 1)
 var p2=Product(PId:"P002", Pname:"ZIP drive", prce: 90.00, qnty: 1)
 var p3=Product(PId:"P003", Pname:"Floppy drive", prce: 50.00, qnty: 1)
 var p4=Product(PId:"P004", Pname:"Monitor", prce: 300.00, qnty: 1)
 var p5=Product(PId:"P005", Pname:"iphone 7 Plus", prce: 1200.00, qnty: 1)
+
+
+var orders=Dictionary<String,String>()
+
+orders.updateValue("1", forKey: "20-02-19")
+orders.updateValue("2", forKey: "20-02-19")
+orders.updateValue("3", forKey: "20-02-19")
 
 
 
